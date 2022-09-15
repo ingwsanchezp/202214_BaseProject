632c3680a4d7d6eac265bf43c24bf7295b9515b0
ingwsanchezp <w.sanchezp@uniandes.edu.co>
2022-09-14 21:45:47 -0500

    Se agrega modulo no guardado

--- src/aerolinea/aerolinea.module.ts
+++ src/aerolinea/aerolinea.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { AerolineaService } from './aerolinea.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
